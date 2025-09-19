d6786b6354455f9d9a1f4ef51cde2395a706c4a8
kanlahac <196105668+kanlahac@users.noreply.github.com>
2025-09-19 16:35:02 -0500

    Create header & about

---
+++ src/components/home/about.js
@@ -0,0 +1,49 @@
+import { SiGodotengine, SiUnity } from "react-icons/si";
+import { PiFileCSharpFill, PiLinkedinLogo } from "react-icons/pi";
+import { RiNextjsFill } from "react-icons/ri";
+import { HiOutlineMail } from "react-icons/hi";
+
+
+export default function About() {
+    return (
+        <div className="my-50">
+            <div className="flex flex-col items-center gap-20">
+
+                <div className="avatar hover-rubberBand">
+                    <div className="mask mask-squircle w-64">
+                        <img src="photo.jpg" />
+                    </div>
+                </div>
+
+                <div className="max-w-sm md:max-w-md lg:max-w-xl text-center">
+
+                    <h2 className="text-5xl font-bold mb-10 hover-swing">Hello!</h2>
+                    <p>My name is <b>Jhony Alexander</b>. Video game developer with 9 years of programming expertise. I'm good at making creative ideas into real games. I love solving tough problems and I always work hard to make great games that people will love.
+                    </p>
+
+                    <h2 className="text-3xl font-bold my-10 hover-swing">SKILLS</h2>
+                    <div className="flex justify-center gap-6 md:gap-8 lg:gap-10">
+                        <SiUnity className="text-5xl hover-rubberBand"/>
+                        <SiGodotengine className="text-5xl hover-rubberBand"/>
+                        <PiFileCSharpFill className="text-5xl hover-rubberBand"/>
+                        <RiNextjsFill className="text-5xl hover-rubberBand"/>
+                    </div>
+
+                </div>
+
+                <div className="flex flex-col md:flex-row gap-6 mt-10 items-center">
+                    <div className="flex items-center gap-2">
+                        <HiOutlineMail className="text-3xl"/>
+                        <p className="font-bold">kanlahac.dev@gmail.com</p>
+                    </div>
+
+                    <a href="https://www.linkedin.com/in/kanlahac/" target="_blank" className="flex items-center gap-2">
+                        <PiLinkedinLogo className="text-3xl"/>
+                        <p className="font-bold">https://www.linkedin.com/in/kanlahac/</p>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
---
+++ src/components/home/header.js
@@ -0,0 +1,21 @@
+export default function Header() {
+
+    
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content text-center">
+
+                <h2 className="text-5xl font-bold animate__animated animate__hinge animate__delay-2s absolute top-50">
+                    WELCOME
+                </h2>
+
+                <div className="max-w-lg">
+                    <h5 className="text-2xl lg:text-3xl animate__animated animate__bounceInDown">Gameplay Developer</h5>
+                    <h1 className="lg:text-8xl md:text-6xl text-4xl font-bold my-5 animate__animated animate__bounceIn">PORTFOLIO</h1>
+                    <p className="text-xl lg:text-2xl animate__animated animate__bounceInUp">Kanlahac</p>
+                </div>
+
+            </div>
+        </div>
+    );
+}
